6c8a7c08dd547949c95d0b9b33e8a22eba6ab7ee
Sander de Bruijn <sander@denofdata.com>
2024-12-27 16:53:46 +0100

    Update fancy site

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
@@ -9,36 +9,13 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/index.html")
-def index2():
-    return render_template("index.html")
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route("/about.html")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/components.html")
-def components():
-    return render_template("components.html")
-
-
-@app.route("/contact.html")
-def contact():
-    return render_template("contact.html")
-
-
-@app.route("/thankyou.html")
-def thankyou():
-    return render_template("tankyou.html")
-
-
-@app.route("/work.html")
-def work():
-    return render_template("work.html")
-
-
-@app.route("/works.html")
-def works():
-    return render_template("works.html")
+@app.route("/submit_form", methods=["POST", "GET"])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+    return render_template("thankyou.html", name=data["yourname"])
